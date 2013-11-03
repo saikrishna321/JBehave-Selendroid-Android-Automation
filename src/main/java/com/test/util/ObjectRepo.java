cc106303697cf084f0b6333b943d97a6f4c0dfdd
sai krishna <saikrishna321@yahoo.com>
2013-11-03 09:58:36 +0530

    Placed all the element ID on the Object Repo

---
+++ src/main/java/com/test/util/ObjectRepo.java
@@ -0,0 +1,12 @@
+package com.test.util;
+
+/**
+ * @author saikrishna
+ * 
+ */
+public class ObjectRepo {
+
+	public static final String assertInputText="my_text_field";
+	public static final String buttonLoginId="signin_btn";
+	public static final String searchField="my_text_field";
+}
